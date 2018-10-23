11afa768fcf06b530260592f54c97da6b7cdb706
Brian McMillan <brian.mcmillan@steeplesquare.co.uk>
2018-10-23 18:10:09 +0100

    Create DMX_Server.py

---
+++ DMX_Server.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+from ola.ClientWrapper import ClientWrapper
+import array
+import sys
+
+from SocketServer import BaseRequestHandler, TCPServer
+wrapper = None
+
+
+def DmxSent(status):
+  if status.Succeeded():
+    print('Success!')
+  else:
+    print('Error: %s' % status.message, file=sys.stderr)
+
+  global wrapper
+  if wrapper:
+    wrapper.Stop()
+
+
+
+DMXData = bytearray(1024)
+
+class EchoHandler(BaseRequestHandler):
+    def handle(self):
+        print('Got connection from', self.client_address)
+        while True:
+            DMXData = self.request.recv(1024)
+            for i in range(10):
+                print ('RECV: ', DMXData[i],':')
+            break
+#        if not DMXData:
+#            break
+        global wrapper
+        wrapper = ClientWrapper()
+        client = wrapper.Client()
+  # send 1 dmx frame
+        client.SendDmx(universe, DMXData[0:512], DmxSent)
+        wrapper.Run()
+
+        
+        self.request.send(b'ACK')
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler)
+    print('Echo server running on port 20000')
+    serv.serve_forever()
